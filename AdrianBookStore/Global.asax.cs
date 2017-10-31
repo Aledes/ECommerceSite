c7357625af6589151b743348c6e269ea24ad4bf8
Aledes <ledadrian@gmail.com>
2017-10-31 11:48:55 -0500

    Created Project

--- AdrianBookStore/Global.asax.cs
+++ AdrianBookStore/Global.asax.cs
@@ -5,7 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+/// <summary>
+///Start of Application
+/// </summary>
 namespace AdrianBookStore
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -14,6 +16,7 @@ namespace AdrianBookStore
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //When app starts, route config is what will "hijack" inbound requests and direct them to your control
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
