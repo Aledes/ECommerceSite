380f1d38b88a3818a067863c864645d2aa97ef67
Adrian Ledesma <ledadrian@gmail.com>
2017-11-13 22:51:06 -0600

    Added Entity Framework

--- AdrianBookStore/Models/Author.cs
+++ AdrianBookStore/Models/Author.cs
@@ -12,22 +12,19 @@ namespace AdrianBookStore.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class Book
+    public partial class Author
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Book()
+        public Author()
         {
-            this.Authors = new HashSet<Author>();
+            this.Books = new HashSet<Book>();
         }
     
-        public int BookID { get; set; }
-        public string Title { get; set; }
-        public Nullable<int> Year { get; set; }
-        public Nullable<decimal> Price { get; set; }
-        public Nullable<int> Quantity { get; set; }
-        public string CoverImage { get; set; }
+        public int AuthorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Author> Authors { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
